ef8f2ff6f84bdf9928248674f3301bee6b17b954
Jamie Catania <catania.jamie@gmail.com>
2017-11-15 19:39:45 -0500

    Finished the course

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyC3C4pHnrvJ7G8-cuQQMF__rIFxAuzoCZg",
+    authDomain: "catch-of-the-day-jamie.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jamie.firebaseio.com",
+});
+
+export default base;
